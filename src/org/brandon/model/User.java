8eb3bfb59035a3d277369cc287f98212656ff157
brandon.cole <brandon.cole@akg-america.com>
2020-04-13 15:45:18 -0400

    Revert "Added age getter and setter."
    This reverts commit ff0bc4aebfc23c96b4ac50eb1fe3761ec8dbf8ce.

--- src/org/brandon/model/User.java
+++ src/org/brandon/model/User.java
@@ -3,14 +3,6 @@ package org.brandon.model;
 public class User {
     private String name;
     private int age;
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public String getName() {
         return name;
     }
